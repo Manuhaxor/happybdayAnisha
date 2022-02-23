9e8db637ea491f60b3ed2f1d20bfe7a94aa8789f
Manu Kumar Nayak <85009514+Manuhaxor@users.noreply.github.com>
2022-02-23 07:29:09 +0530

    update animation.js

--- js/animation.js
+++ js/animation.js
@@ -172,7 +172,7 @@ button.addEventListener("click",function(){
             setTimeout(() => {
                 frames[0].classList.add("appear");
                 frames[0].style.opacity = "1";
-            }, 3000);
+            }, 1000);
             return;
         }
 
